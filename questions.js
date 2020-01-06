450fe5bb923fab6bfdb34e52182982620ae8ac48
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-06 15:33:22 -0600

    intial commit of quiz

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Commonly used data tyes DO NOT includes",
+        choices: ["strings","booleans","alerts","numbers"],
+        answer: "alerts",
+    },
+    {
+        title: "The condition in an if/else statement is enclosed by____",
+        choices: ["quotes","curly brackets","parentheses","square brackets"],
+        answer: "parentheses",
+    },
+    {
+        title: "arrays can be used to store",
+        choices: ["","","",""],
+        answer: "",
+    },
+    {
+        title: "",
+        choices: ["","","",""],
+        answer: "",
+    },
+    {
+        title: "",
+        choices: ["","","",""],
+        answer: "",
+    },
+
+];
---
+++ script.js
@@ -0,0 +1,50 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+      
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+var score = 0;
+
+for(var i = 0; i < questions.length; i++){
+     var response = window.prompt(questions[i].prompt);
+     if(response == questions[i].answer){
+          score++;
+          alert("Correct!");
+     } else {
+          alert("WRONG!");
+     }
+}
+alert("you got " + score + "/" + questions.length);
