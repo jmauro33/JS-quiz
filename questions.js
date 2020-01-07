841c18a717ae148bfad787d83078e7fb8c7e42d0
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-07 15:01:26 -0600

    updated commit of js

--- questions.js
+++ questions.js
@@ -5,24 +5,24 @@ var questions = [
         answer: "alerts",
     },
     {
-        title: "The condition in an if/else statement is enclosed by____",
+        title: "What does CSS stand for",
+        choices: ["Cascading Style Sheets","Color Sizing Styles","Common Syntax Source","Coding Source Software"],
+        answer: "Cascading Style Sheets",
+    },
+    {
+        title: "The conditon of an if/else statement muse be enclosed using",
         choices: ["quotes","curly brackets","parentheses","square brackets"],
         answer: "parentheses",
     },
     {
-        title: "arrays can be used to store",
-        choices: ["","","",""],
-        answer: "",
+        title: "Arrays in Javascript can store ",
+        choices: ["numbers and strings","other arrays","all of the above","booleans"],
+        answer: "all of the above",
     },
     {
-        title: "",
-        choices: ["","","",""],
-        answer: "",
-    },
-    {
-        title: "",
-        choices: ["","","",""],
-        answer: "",
+        title: "What does HTML stand for",
+        choices: ["hypertext markup language","href manual type link","hyper type mode line","heading markup tracking language"],
+        answer: "hypertext markup language",
     },
 
 ];
--- script.js
+++ script.js
@@ -1,50 +1,31 @@
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
-    this.questionIndex = 0;
+    this.questionIndex = 5;
 }
  
 Quiz.prototype.getQuestionIndex = function() {
     return this.questions[this.questionIndex];
 }
+function showQuestions()
+    getElementById(questions)
 
-
-function populate() {
-    if(quiz.isEnded()) {
-        showScores();
-    }
-    else {
-      
-        var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
-
-        var choices = quiz.getQuestionIndex().choices;
-        for(var i = 0; i < choices.length; i++) {
-            var element = document.getElementById("choice" + i);
-            element.innerHTML = choices[i];
-            guess("btn" + i, choices[i]);
-        }
- 
-        showProgress();
-    }
-};
- 
-function guess(id, guess) {
-    var button = document.getElementById(id);
-    button.onclick = function() {
-        quiz.guess(guess);
-        populate();
-    }
-};
-var score = 0;
-
-for(var i = 0; i < questions.length; i++){
-     var response = window.prompt(questions[i].prompt);
-     if(response == questions[i].answer){
-          score++;
-          alert("Correct!");
-     } else {
-          alert("WRONG!");
-     }
+function submitQuiz(){
+    console.log('submitted')
 }
-alert("you got " + score + "/" + questions.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
