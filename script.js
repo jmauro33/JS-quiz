dd91b588c8bf8f59c10572707321c488978c5b8b
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-10 11:56:22 -0600

    hope this works

--- script.js
+++ script.js
@@ -1,31 +1,101 @@
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 5;
-}
- 
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
-}
-function showQuestions()
-    getElementById(questions)
+//variables time left, high scores(array) index of current questions
+// var questions
+// start quiz 
+
+function startQuiz(){
+
+document.getElementById("start")
+
+    start.addEventListener("click",start)   
+
+
+};
+
+//show first question
+
+
+
+
+//display question
+
+var currentQuestionIndex = 0;
+
+function showQuestions(){
+    console.log('called showQuestions')
+
+     document.getElementById("quiz").textContent = questions[currentQuestionIndex].title
+
+};    
+
+
+// show the answers
+
+
+function showAnswers(choices){
+    console.log('called showAnswers')
+
+    document.getElementById("choices").addEventListener("click", function() {
+
+
+
+//click functions to move between questions
+
+function nextQuestion(nextQuestion){
+    currentQuestionIndex++
+    showQuestions()
+};
+
+var incrementEl = document.querySelector("#next");
+
+incrementEl.addEventListener("click", nextQuestion);
+
+
+function previousQuestion(previousQuestion){
+    currentQuestionIndex--
+    showQuestions()
+};
+console.log('called previousQuestion')
+
+var decrementEl = document.querySelector("#previous");
+
+decrementEl.addEventListener("click", previousQuestion); 
+      
 
 function submitQuiz(){
+
+
     console.log('submitted')
-}
+
+document.getElementById("submit")
 
 
+    submit.addEventListener("click", submitQuiz);
+     
+
+};
 
 
+function answerScore(questions){
+    var questions =
+    document.getElementById(questions)
+};
 
 
+//toggle timer
 
+//hide everything unncessary
 
+//score the answer
+//feedback box
+//if wrong subtract time
 
+//timer ticks
+//decremnt time left
+//update display
 
-
-
-
-
-
-
+//show high showScores
+//display scores
+//hide unneccesary stuff
+//display scores after completing questions
+//if done display score
+        
