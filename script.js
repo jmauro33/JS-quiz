1eda27102c6ecb82134b05cf293d7e10c49195c1
jmauro33 <jonathanmauro16@yahoo.com>
2020-01-10 13:19:13 -0600

    added hopelfy working code

--- script.js
+++ script.js
@@ -2,75 +2,67 @@
 
 // start quiz 
 
-function startQuiz(){
+function startQuiz() {
 
-document.getElementById("start")
+    document.getElementById("start")
 
-    start.addEventListener("click",start)   
+    start.addEventListener("click", start)
 };
 
 //display questions
 
 var currentQuestionIndex = 0;
 
-function showQuestions(){
-    
-    document.getElementById("quiz").textContent = questions[currentQuestionIndex].title
-
-};    
-console.log('called showQuestions')
-
-// show the answers
-
-function nextQuestion(){
-    currentQuestionIndex++
-    showQuestions()
+function showQuestions() {
+   
 };
-console.log('called nextQuestion')
-var incrementEl = document.querySelector("#next");
+    console.log('called showQuestions')
+ var counter = 0;
+    for (var i = 0; i < questions.length; i++) {
+        var answer = prompt(questions[i][0]);
+        if (answer === questions[i][1]) {
+            correctAnswers.push([questions[i][0]]);
+            counter += 1;
+        } else {
+            wrongAnswers.push([questions[i][0]]);
+        }
+  document.getElementById("quiz").textContent = questions[currentQuestionIndex].title
 
-incrementEl.addEventListener("click", nextQuestion);
 
 
-function previousQuestion(){
-    currentQuestionIndex--
-    showQuestions()
-};
-console.log('called previousQuestion')
 
-var decrementEl = document.querySelector("#previous");
 
-decrementEl.addEventListener("click", previousQuestion); 
+function nextQuestion() {
+        currentQuestionIndex++
+        showQuestions()
+    };
+    console.log('called nextQuestion')
+    var incrementEl = document.querySelector("#next");
 
-      
+    incrementEl.addEventListener("click", nextQuestion);
+
+
+function previousQuestion() {
+        currentQuestionIndex--
+        showQuestions()
+    };
+    console.log('called previousQuestion')
+
+    var decrementEl = document.querySelector("#previous");
+
+    decrementEl.addEventListener("click", previousQuestion);
+
+
 
 function submitQuiz(){
 
-document.getElementById("submit")
-    submit.addEventListener("click", submitQuiz);
+        document.getElementById("submit")
+        submit.addEventListener("click", submitQuiz);
+    };
+    console.log('submitted')
+
+function answerScore(questions) {
+        var questions =
+            document.getElementById(questions)
 };
-console.log('submitted')
-
-function answerScore(questions){
-    var questions =
-    document.getElementById(questions)
-};
-
-
-//toggle timer
-
-//hide everything unncessary
-
-//score the answer
-//feedback box
-//if wrong subtract time
-
-//timer ticks
-//decremnt time left
-//update display
-
-//show high showScores
-//display scores
-//hide unneccesary stuff
-//display scores after completing questions
-//if done display score
+}
